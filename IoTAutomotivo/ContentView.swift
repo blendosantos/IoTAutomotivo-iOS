0dcdec84f8f9ce6b84fa9c1a295986c13c249667
Blendo Santos <blendo@outlook.com>
2020-12-09 22:21:17 -0300

    Initial Commit

---
+++ IoTAutomotivo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IoTAutomotivo
+//
+//  Created by Blendo Santos on 09/12/20.
+//  Copyright © 2020 Blendo Santos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
